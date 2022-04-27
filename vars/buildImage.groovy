41b4d79f796611b2a06830b0f57d8f855a7292c4
Marvin Korir <marvinkorir.mk@gmail.com>
2022-04-27 12:46:46 +0300

    update buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building Image of the application'
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-        sh 'docker build -t 127.0.0.1:8083/java-maven-app:1.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin 127.0.0.1:8083'
     }
 }
